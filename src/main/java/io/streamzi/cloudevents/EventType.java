0b57a8a0d2221435df0dc00611859b967c9c6ac6
Matthias Wessendorf <matzew@apache.org>
2018-06-04 20:24:58 +0200

    first event type qualifier draft, and some more API

---
+++ src/main/java/io/streamzi/cloudevents/EventType.java
@@ -0,0 +1,14 @@
+package io.streamzi.cloudevents;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Target({ ElementType.PARAMETER, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventType {
+    String name() default "";
+}
---
+++ src/main/java/io/streamzi/cloudevents/EventTypeQualifier.java
@@ -0,0 +1,18 @@
+package io.streamzi.cloudevents;
+
+import javax.enterprise.util.AnnotationLiteral;
+
+public class EventTypeQualifier extends AnnotationLiteral<EventType> implements EventType {
+
+    private static final long serialVersionUID = 1L;
+    private final String eventName;
+
+    public EventTypeQualifier(final String name) {
+        eventName=name;
+    }
+
+    @Override
+    public String name() {
+        return eventName;
+    }
+}
