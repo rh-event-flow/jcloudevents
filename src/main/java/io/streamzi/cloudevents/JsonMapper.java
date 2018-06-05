060f2aa16e2709925fc7c803e59670dec40a106b
Matthias Wessendorf <matzew@apache.org>
2018-06-05 10:27:44 +0200

    Adding JAX-RS endpoint

--- src/main/java/io/streamzi/cloudevents/JsonMapper.java
+++ src/main/java/io/streamzi/cloudevents/JsonMapper.java
@@ -1,11 +1,11 @@
 package io.streamzi.cloudevents;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.streamzi.cloudevents.impl.CloudEventImpl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.ZonedDateTime;
 import java.util.logging.Logger;
 
 public final class JsonMapper {
@@ -13,6 +13,9 @@ public final class JsonMapper {
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final Logger LOGGER = Logger.getLogger(JsonMapper.class.getName());
 
+    {
+        MAPPER.registerModule(new Jdk8Module());
+    }
 
     public static CloudEventImpl fromInputStream(final InputStream inputStream) {
         try {
@@ -21,9 +24,5 @@ public final class JsonMapper {
             LOGGER.severe(e.getMessage());
             throw new IllegalStateException("input was not parseable", e);
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(ZonedDateTime.parse("2018-04-23T12:28:22.4579346Z"));
     }
 }
